4f8a45357d38463275e00928cc3eb4be9ff9ccf3
DerekR <roes0017@algonquincollege.com>
2014-09-25 16:54:43 -0400

    "final push for 0.1"

--- My First Dynamic Web Project/src/com/amzi/servlets/RegisterServlet.java
+++ My First Dynamic Web Project/src/com/amzi/servlets/RegisterServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;  
 import javax.servlet.http.HttpSession;
 
-import com.amzi.dao.Login;
 import com.amzi.dao.Register; 
 
 public class RegisterServlet extends HttpServlet {
