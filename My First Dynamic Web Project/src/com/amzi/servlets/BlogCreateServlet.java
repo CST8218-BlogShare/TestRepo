80202bc19c2431e98503a797bbccf9df29cc4bef
litw0005 <litw0005@algonquinlive.com>
2014-10-26 20:47:03 -0400

    minor fixes to blogcreate to get it working for me

--- My First Dynamic Web Project/src/com/amzi/servlets/BlogCreateServlet.java
+++ My First Dynamic Web Project/src/com/amzi/servlets/BlogCreateServlet.java
@@ -6,17 +6,20 @@ import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;  
 import javax.servlet.ServletException;  
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;  
 import javax.servlet.http.HttpServletRequest;  
 import javax.servlet.http.HttpServletResponse;  
 import javax.servlet.http.HttpSession;  
   
 
+
 import com.amzi.dao.Blog;
 import com.amzi.dao.Post;
 import com.amzi.dao.PostEditPrivilege;
 import com.amzi.dao.User;
-  
+
+@WebServlet("/BlogCreateServlet")
 public class BlogCreateServlet extends HttpServlet{  
   
 	 private static final long serialVersionUID = 1L;
